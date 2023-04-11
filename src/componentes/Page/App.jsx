440b27741ed8d372b3d8fc769299d23bb981bb0e
IngridsSilveira <ingridsouzaok@gmail.com>
2023-04-11 11:58:50 -0300

    preloader add

--- src/componentes/Page/App.jsx
+++ src/componentes/Page/App.jsx
@@ -3,6 +3,14 @@ import Header from '../Header/Header'
 export default function App() {
   return (
     <div className="bg-div bg-black">
+    {/* <div id="preloader">
+        <h1>Loading</h1>
+        <p class="c-loader"></p>
+        <p class="c-loader"></p>
+        <p class="c-loader"></p>
+        <p class="c-loader"></p>
+        <p class="c-loader"></p>
+    </div> */}
       <Header />
     </div>
   );
