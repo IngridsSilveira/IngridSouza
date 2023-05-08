2f89e0ed197a7e44bd29897beeece7e398abeb9b
IngridsSilveira <ingridsouzaok@gmail.com>
2023-05-08 10:42:55 -0300

    new form contact

--- src/componentes/Body/Contacts/index.jsx
+++ src/componentes/Body/Contacts/index.jsx
@@ -1,6 +1,33 @@
+import {useState} from 'react' 
+import emailjs from '@emailjs/browser'
 import "./styles.scss";
 
+
 export default function Contacs() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [message, setMessage] = useState('')
+
+
+    const sendEmail = (evt) => {
+        evt.preventDefault();
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+        emailjs.send('service_0zhsa8g', 'template_q5qojeb', templateParams, 'YJeBNk_Ug7kAKFDLz')
+        .then((res)=> {
+            alert("E-MAIL ENVIADO COM SUCESSO");
+            setName('')
+            setEmail('')
+            setMessage('')
+        }, (err) => {
+            console.log('Error', err);
+        })
+    }
+
   return (
     <>
       <section id="Contact" className="fSection hidden">
@@ -11,25 +38,27 @@ export default function Contacs() {
             <p className="description hidden" id="wantContact">Want to contact me?</p>
             <p className="form_description hidden" id="preenchaContact">Fill out the form and I will contact you as soon as
                 possible.</p>
-            <form id="form" action="https://wa.me/5521979661084?text=Tenho%20interesse%20em%seus%20sites" className="form hidden">
+
+            <form id="form" className="form hidden" onSubmit={sendEmail}>
                 <fieldset>
                     <div className="input-container">
                         <input name="name" id="nome" className="input form-control" type="text" placeholder="Name:"
-                            data-tipo="nome" minLength="6" required/>
+                            data-tipo="nome" minLength="6" onChange={(e)=>setName(e.target.value)} value={name} required/>
                         <span className="input-mensagem-erro"></span>
                     </div>
                     <div className="input-container">
                         <input name="email" id="email" className="input form-control" type="email" placeholder="E-mail:"
-                            data-tipo="email" required/>
+                            data-tipo="email" onChange={(e)=>setEmail(e.target.value)} value={email} required/>
                         <span className="input-mensagem-erro"></span>
                     </div>
                     <div className="input-container">
-                        <textarea name="message" id="mensagem" className="input mensagem form-control" cols="30" rows="10"
-                            placeholder="Message:" data-tipo="mensagem" minLength="50" required></textarea>
+                        <textarea name="message" id="mensagem" className="form-control mb-3" rows="3"
+                            placeholder="Message:" data-tipo="mensagem" minLength="50" onChange={(e)=>setMessage(e.target.value)} value={message} required></textarea>
                         <span className="input-mensagem-erro"></span>
                     </div>
                     <input id="submit" type="submit" className="btn btn-success btn_contact" value="Send" target='_blank'/>
                 </fieldset>
+
             </form>
       </section>
     </>
